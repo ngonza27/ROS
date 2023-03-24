41d1b2208f97ddfab1f11efae4f7f4914329ce5f
Nicolas Gonzalez <ngonzaa27@gmail.com>
2023-03-24 14:14:49 +0000

    tutorial + gdp steering proposal

---
+++ src/my_robot_controller/scripts/pose_subscriber.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import rospy
+from turtlesim.msg import Pose
+
+def pose_callback(msg: Pose):
+	rospy.loginfo(f"({msg.x}, {msg.y})")
+
+if __name__ == '__main__':
+	rospy.init_node("turtle_pose_subscriber")
+	sub=rospy.Subscriber("/turtle1/pose", Pose, callback=pose_callback)
+	rospy.loginfo("Node has been started")
+	rospy.spin()
